f2da02e14e1d1dfd80ea10ead01aba0df00fd4cf
AkitoKawaguchi <amkaacl07@gmail.com>
2024-06-07 21:37:33 +0900

    delete

--- db/migrate/20240605031556_change_column_to_user.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToUser < ActiveRecord::Migration[7.1]
-  def up
-    change_column :users, :id, :string
-  end
-
-  def down
-    change_column :users, :id, :integer
-  end
-end
--- db/migrate/20240605034258_change_column_to_user2.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToUser2 < ActiveRecord::Migration[7.1]
-  def up
-    change_column :users, :id, :integer
-  end
-
-  def down
-    change_column :users, :id, :string
-  end
-end
